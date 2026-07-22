8bdd765d4d82ab5ad1223193698e0a9b5d0b69bf
deepdubai232 <redlotus365a@gmail.com>
2026-07-22 16:55:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+// ===========================
+// VIP CONFIG
+// ===========================
+
+const VIP_CONFIG = {
+
+    // Channel Name
+    channelName: "GOLDY CRICKET 🏆",
+    // Loader Text
+    loaderText: "GOLDY CRICKET",
+
+    // Telegram Link
+    telegramLink: "https://t.me/Ranwa677",
+
+    // Profile Image
+    profileImage: "profile.jpg",
+
+    // Page Title
+    pageTitle: "GOLDY CRICKET",
+
+    // Heading
+    heading: "GOLDY CRICKET",
+
+    // Badge
+    badgeText: "VIP CHANNEL",
+
+    // Description
+    description: "Accurate Analysis • Match Update • Right Prediction",
+
+    // Button Text
+    buttonText: "JOIN TELEGRAM FOR NEW UPDATE",
+
+    // Countdown
+    countdownTime: 30,
+
+    // Theme
+    goldColor: "#FFD700",
+    glowColor: "#ff6600"
+
+};
+document.documentElement.style.setProperty('--gold', VIP_CONFIG.goldColor);
+document.documentElement.style.setProperty('--glow', VIP_CONFIG.glowColor);
+console.log("VIP CONFIG LOADED");
